ebb84f2c63554b21a19ee0232d1f378624e5a0d1
Adam M Ludvik <adam.m.ludvik@gmail.com>
2018-02-03 00:18:49 -0600

    Test io polling

--- game/src/main.rs
+++ game/src/main.rs
@@ -4,7 +4,10 @@ extern crate rpg_engine;
 mod syllable_sets;
 
 
+use std::{thread, time};
+
 use rpg_engine::generate::name::NameGenerator;
+use rpg_engine::io::screen::Screen;
 
 use syllable_sets::simple_fantasy;
 
@@ -13,4 +16,19 @@ fn main() {
     let ng = NameGenerator::new(&syllables);
     let name = ng.generate(3, 7);
     println!("{}{}", &name[0..1].to_uppercase(), &name[1..]);
+
+    let screen = Screen::new();
+    let win = screen.window(0, 0, 10, 3);
+    loop {
+        match screen.poll() {
+            Some(ch) => {
+                win.putc(ch);
+            },
+            None => {
+                win.putc('.' as u32);
+                thread::sleep(time::Duration::from_millis(100));
+            }
+        };
+        win.refresh();
+    }
 }
