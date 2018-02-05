6e68da2450fbf82db6f6c6a505565f778f4742fb
Adam M Ludvik <adam.m.ludvik@gmail.com>
2018-02-04 20:07:42 -0600

    Provide default implementations

--- engine/src/world/scene.rs
+++ engine/src/world/scene.rs
@@ -2,8 +2,8 @@ use io::window::Window;
 
 pub trait Scene {
     // Called periodically to update the scene
-    fn update(&mut self);
+    fn update(&mut self) {}
 
     // Called to draw the scene to a window
-    fn draw(&self, window: &Window);
+    fn draw(&self, window: &Window) {}
 }
