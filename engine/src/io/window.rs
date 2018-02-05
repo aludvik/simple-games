78d7cdeec4dc8c97e6f2907199404b8267a31936
Adam M Ludvik <adam.m.ludvik@gmail.com>
2018-02-04 19:52:49 -0600

    Add some methods to window

--- engine/src/io/window.rs
+++ engine/src/io/window.rs
@@ -13,6 +13,14 @@ impl Window {
         ncurses::waddch(self.window, ch as u32);
     }
 
+    pub fn mvputc(&self, x: u32, y: u32, ch: char) {
+        ncurses::mvwaddch(self.window, y as i32, x as i32, ch as u32);
+    }
+
+    pub fn clear(&self) {
+        ncurses::wclear(self.window);
+    }
+
     pub fn refresh(&self) {
         ncurses::wrefresh(self.window);
     }
