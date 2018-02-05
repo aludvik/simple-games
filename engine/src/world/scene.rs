f9a78e1db826ef7da88590c1cfa4a2512a854612
Adam M Ludvik <adam.m.ludvik@gmail.com>
2018-02-04 20:08:00 -0600

    Extend scene interface

--- engine/src/world/scene.rs
+++ engine/src/world/scene.rs
@@ -6,4 +6,13 @@ pub trait Scene {
 
     // Called to draw the scene to a window
     fn draw(&self, window: &Window) {}
+
+    // Called when a key is pressed (when this scene is active)
+    fn on_pressed(&mut self, key: char) {}
+
+    // Called when this scene gains focus
+    fn on_focus(&mut self) {}
+
+    // Called when this scene loses focus
+    fn on_unfocus(&mut self) {}
 }
